6c3961f15a42b9f8392990117f0b61a00f0a65dc
TanguyBader <33072659+TanguyBader@users.noreply.github.com>
2017-12-14 14:10:15 -0800

    Update webscraping program python.py
    put in twillio on line 32 and 7

--- webscraping program python.py
+++ webscraping program python.py
@@ -4,6 +4,18 @@ import time
 import urllib2
 from bs4 import BeautifulSoup
 
+from twilio.rest import Client
+from datetime import datetime
+
+account_sid = 'XXX'
+auth_token = 'XXX'
+twilio_phone_number = 'XXX'
+my_phone_number = 'XXX'
+
+client = Client(account_sid, auth_token)
+ 
+def main ():
+
 def main():
   while True:
     quote_page = 'https://tekka332510898.wordpress.com/'
@@ -16,6 +28,13 @@ def main():
     price = price_box.text
     print price  
     time.sleep(20)
+
+    #Text your the information.
+		body = name + ', ' + price + ', ' + str(datetime.now())
+		client . messages.create(
+			body=body,
+			to=my_phone_number,
+			from_=twilio_phone_number
   
 if __name__ == "__main__":
   main()
